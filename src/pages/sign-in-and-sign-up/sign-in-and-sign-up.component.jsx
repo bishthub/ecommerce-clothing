90ba483aa428f8e7ad4f159fe4851de0d1e4f3d5
Bishtji <bishtjisop@gmail.com>
2020-05-16 21:49:01 +0530

    Add signup component and link with sign in

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./sign-in-and-sign-up.style.scss";
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.components";
+
 
 const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'><SignIn />
+    <div className='sign-in-and-sign-up'>
+    <SignIn />
+    <SignUp />
     </div>
 );
 
